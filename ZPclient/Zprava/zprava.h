1938c38896290bb9825001617f128ebe31ec0b5e
kian behzad <kian.behzad@gmail.com>
2018-05-21 12:17:43 +0430

    bring all form widgets into the class

--- ZPclient/Zprava/zprava.h
+++ ZPclient/Zprava/zprava.h
@@ -3,6 +3,22 @@
 
 #include <QMainWindow>
 #include <QPushButton>
+#include <QTextEdit>
+#include <QLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QApplication>
+#include <QFile>
+#include <QString>
+#include <QLatin1String>
+#include <QDebug>
+#include <QDir>
+#include <QDebug>
+#include <QDesktopWidget>
+
 
 class Zprava : public QMainWindow
 {
@@ -11,6 +27,49 @@ class Zprava : public QMainWindow
 public:
     Zprava(QWidget *parent = 0);
     ~Zprava();
+//GUI
+public:
+    //STYLESHEET
+    QFile File;
+    QString StyleSheet;
+    //SIGNUP FORM WIDGETS
+    QLabel* signup_icon_label;
+    QPixmap* signup_icon_map;
+    QHBoxLayout* signup_icon_lay;
+    QWidget* signup_icon_widg;
+    QLabel* signup_topic_label;
+    QHBoxLayout* signup_topic_lay;
+    QWidget* signup_topic_widg;
+    QLineEdit* signup_email_text;
+    QLineEdit* signup_id_text;
+    QLineEdit* signup_pass_text;
+    QPushButton* signup_button;
+    QHBoxLayout* signup_button_lay;
+    QWidget* signup_button_widg;
+    QVBoxLayout* signup_form_lay;
+    QWidget* signup_form_widg;
+    //LOGIN FORM WIDGETS
+    QLabel* login_icon_label;
+    QPixmap* login_icon_map;
+    QHBoxLayout* login_icon_lay;
+    QWidget* login_icon_widg;
+    QLabel* login_topic_label;
+    QHBoxLayout* login_topic_lay;
+    QWidget* login_topic_widg;
+    QLineEdit* login_id_text;
+    QLineEdit* login_pass_text;
+    QPushButton* login_button;
+    QHBoxLayout* login_button_lay;
+    QWidget* login_button_widg;
+    QVBoxLayout* login_form_lay;
+    QWidget* login_form_widg;
+    //WHOLE FORM WIDGETS
+    QHBoxLayout* whole_lay;
+    QWidget* whole_widg;
+
+
+
+
 };
 
 #endif // ZPRAVA_H
