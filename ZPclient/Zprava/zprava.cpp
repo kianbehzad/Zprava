2b675ca917ef1818f8bf47805f01477720c6daf5
kian behzad <kian.behzad@gmail.com>
2018-06-01 13:52:13 +0430

    every class has his own qss file now

--- ZPclient/Zprava/zprava.cpp
+++ ZPclient/Zprava/zprava.cpp
@@ -6,7 +6,6 @@
 Zprava::Zprava(QWidget *parent)
     : QMainWindow(parent)
 {
-    //SET THE FORM
     //resizing main window
     int width{1280}, height{939};
     if(QApplication::desktop()->availableGeometry().width() < 1280)
@@ -14,6 +13,10 @@ Zprava::Zprava(QWidget *parent)
     if(QApplication::desktop()->availableGeometry().height() < 939)
         height = QApplication::desktop()->availableGeometry().height();
     setFixedSize(width, height);
+
+    //stylesheet
+    apply_stylesheet();
+
     //add form into main window
     form = new ZpForm(true);
     QHBoxLayout* lay = new QHBoxLayout();
@@ -29,4 +32,14 @@ Zprava::Zprava(QWidget *parent)
 Zprava::~Zprava()
 {
 
+}
+
+void Zprava::apply_stylesheet()
+{
+    //getting style sheets
+    File.setFileName(":/qApp_stylesheet.qss");
+    qDebug() << "is qt qApp_stylesheet opend: " <<File.open(QFile::ReadOnly);
+    FormStyleSheet = QLatin1String(File.readAll());
+    this->setStyleSheet(FormStyleSheet);
+    File.close();
 }
