41b8330bf6049d7ddee7aa50d0ce060ee576d481
kian behzad <kian.behzad@gmail.com>
2018-07-17 02:02:09 +0430

    a better solution for contactlist unstaging(in thread)

--- ZPclient/Zprava/zpcontactlist.cpp
+++ ZPclient/Zprava/zpcontactlist.cpp
@@ -25,7 +25,7 @@ ZpContactList::ZpContactList(QScrollArea *parent) : QScrollArea(parent)
     this->setContentsMargins(0, 0, 0, 0);
 
     data_thread = new ZpContactList_Thread();
-    connect(data_thread, SIGNAL(gotData(QList<QString>)), this, SLOT(handle_gotData(QList<QString>)));
+    connect(data_thread, SIGNAL(gotData(QList<QString>,QList<QString>)), this, SLOT(handle_gotData(QList<QString>,QList<QString>)));
 
 }
 
@@ -77,33 +77,19 @@ void ZpContactList::handle_update()
     this->sort();
 }
 
-void ZpContactList::handle_gotData(QList<QString> contacts)
+void ZpContactList::handle_gotData(QList<QString> username, QList<QString> deleted)
 {
-    bool is_correct_data{!contacts.isEmpty()};
-    if(is_correct_data)
+    for(const auto& contact: username)
+        this->add_contact(contact);
+    for(const auto& del: deleted)
     {
-        for(const auto& last: last_contacts)
-        {
-            bool exist{false};
-            for(const auto& _new: contacts)
-                if(last == _new)
-                    exist = true;
-            if(!exist)
-            {
-                contacts_list_layout->removeWidget(get_contact(last));
-                get_contact(last)->hide();
-                for(const auto& c: contacts_list)
-                    if(c->user->username == last)
-                        contacts_list.removeOne(c);
-                //delete get_contact(last);
-            }
-        }
-        last_contacts.clear();
-        for(const auto& contact: contacts)
-        {
-            this->add_contact(contact);
-            last_contacts.push_back(contact);
-        }
+        qDebug() << del;
+        ZpContact* tmp = get_contact(del);
+        contacts_list_layout->removeWidget(tmp);
+        tmp->hide();
+        for(int i{}; i < contacts_list.size(); i++)
+            if(contacts_list[i]->user->username == del)
+                contacts_list.removeAt(i);
     }
 }
 
